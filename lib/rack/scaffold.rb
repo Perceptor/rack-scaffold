0de47d5296eaeabfe14510ba40c4ca0c1a36744b
Dunya Kirkali <dunyakirkali@gmail.com>
2014-01-28 12:45:36 +0100

    ?

--- lib/rack/scaffold.rb
+++ lib/rack/scaffold.rb
@@ -132,7 +132,7 @@ module Rack
             last_modified(last_modified_time(resource, record)) if resource.timestamps?
             
             json_hash = {"#{resource.singular}" => JSON.parse(record.to_json(:except=>[:deviceToken]))}
-            json_hash[:completed_credits] = Session.find(params[:id]).completed_credits?
+            json_hash[:completed_credits] = record.hits.map(&:completed).all?
             json_hash.to_json
           end
 
